53a402b2a16e36486e1b53e1e1c66e9c46a3b7ec
Julia Fitzer <fitzerjulie@yahoo.com>
2020-07-12 15:23:25 -0400

    react router paramters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import CampsiteInfo from './CampsiteInfoComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -33,6 +34,15 @@ class Main extends Component {
             );
         }
 
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };   
+
         return (
             <div>
                 <Header />
@@ -40,6 +50,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
@@ -47,5 +58,6 @@ class Main extends Component {
         );
     };
 }
+
 
 export default Main;
